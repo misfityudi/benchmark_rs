c9a7e038853b92dcb45ea4fa5f8306b98f6f4bca
Vikas Chahar <misfityudi@gmail.com>
2025-03-03 16:35:47 +0530

    add lib crate, add operation, collection enums, add target struct with new associated function, add tests for new

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+#[derive(Debug, PartialEq)]
+pub enum Operation {
+    Insert,
+    Lookup,
+    Delete,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Collection {
+    Vector,
+    HashMap,
+    HashSet,
+    BTreeMap,
+    BTreeSet,
+}
+
+pub struct Target {
+    pub operation: Operation,
+    pub collection: Collection,
+    pub num_of_items: i32,
+}
+
+impl Target {
+    pub fn new(operation: Option<Operation>, collection: Option<Collection>, num_of_items: Option<i32>) -> Self {
+        Self {
+            operation: operation.unwrap_or(Operation::Insert),
+            collection: collection.unwrap_or(Collection::Vector),
+            num_of_items: num_of_items.unwrap_or(1_000_000),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn default_benchmark() {
+        let target = Target::new(None, None, None);
+        assert_eq!(target.operation, Operation::Insert);
+        assert_eq!(target.collection, Collection::Vector);
+        assert_eq!(target.num_of_items, 1_000_000);
+    }
+
+    #[test]
+    fn custom_benchmark() {
+        let target = Target::new(Some(Operation::Lookup), Some(Collection::HashMap), Some(1_000));
+        assert_eq!(target.operation, Operation::Lookup);
+        assert_eq!(target.collection, Collection::HashMap);
+        assert_eq!(target.num_of_items, 1_000);
+    }
+}
