92dd8bbd7c5619ec6f5a89e93f4f23fee7adff30
Vikas Chahar <misfityudi@gmail.com>
2025-03-08 19:39:26 +0530

    start implementing to show all insert, lookup and delete if there is no operation given

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,11 +1,11 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Operation {
     Insert,
     Lookup,
     Delete,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Collection {
     Vec,
     HashMap,
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,33 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let collection = args
-        .get(1)
-        .map(|arg| arg.to_string())
-        .unwrap_or_else(|| "vec".to_string());
-    let operation = args
-        .get(2)
-        .map(|arg| arg.to_string())
-        .unwrap_or_else(|| "insert".to_string());
-    let num_of_items = args
-        .get(3)
-        .and_then(|arg| arg.parse::<i32>().ok())
-        .unwrap_or(1_000_000);
+    let collection = args.get(1).map(|arg| arg.to_string());
+    let operation = args.get(2).map(|arg| arg.to_string());
+    let num_of_items = args.get(3).and_then(|arg| arg.parse::<i32>().ok()).unwrap_or(100_000);
 
-    let collection_enum = benchmark::collection(collection);
-    let operation_enum = benchmark::operation(operation);
+    if collection.is_none() || operation.is_none() {
+        // Default to running all operations on all collections
+        let collections = vec![
+            benchmark::collection("vec".to_string())
+        ];
 
-    let target = Target::new(
-        Some(collection_enum),
-        Some(operation_enum),
-        Some(num_of_items),
-    );
-    target.benchmark();
+        let operations = vec![
+            benchmark::operation("insert".to_string()),
+            benchmark::operation("lookup".to_string()),
+            benchmark::operation("delete".to_string()),
+        ];
+
+        for collection_enum in collections {
+            for operation_enum in &operations {
+                let target = Target::new(Some(collection_enum), Some(*operation_enum), Some(num_of_items));
+                target.benchmark();
+            }
+        }
+    } else {
+        let collection_enum = benchmark::collection(collection.unwrap());
+        let operation_enum = benchmark::operation(operation.unwrap());
+
+        let target = Target::new(Some(collection_enum), Some(operation_enum), Some(num_of_items));
+        target.benchmark();
+    }
 }
